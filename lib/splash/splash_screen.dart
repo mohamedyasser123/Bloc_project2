ddc327171c6e45fb5570cce3ac419980b3d0f2a0
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2024-12-06 13:06:45 +0200

    first commit

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -5,11 +5,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../home/signup_screen.dart';
 import 'bloc/splash_blocs.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  PageController pageController=PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,34 +28,39 @@ class SplashScreen extends StatelessWidget {
               alignment: Alignment.topCenter,
               children: [
                 PageView(
+                  controller: pageController,
                   onPageChanged: (index){
                     BlocProvider.of<SplashBloc>(context).add(PageChanged(index));
-
-
                   },
                   children: [
                     _page(
+                        pageController,
                         1,
                         "First See Learning",
                         "Forget about a for of paper all Knowlage in one learing",
                         "Next",
-                        ""
+                        "assets/images/reading.png",
+                      context
 
 
                     ),
                     _page(
+                        pageController,
                         2,
                         "Connect With Everyone",
                         "Always keep in touch with your tutor & friends.let's get connected",
                         "Next",
-                        ""
+                        "assets/images/boy.png",
+                        context
                     ),
                     _page(
+                        pageController,
                         3,
                         "Get Started",
                         "Let's Started",
                         "Started",
-                        ""
+                        "assets/images/man.png",
+                        context
                     ),
 
                   ],
@@ -78,11 +90,15 @@ class SplashScreen extends StatelessWidget {
     );
   }
 }
-Widget _page( int index,String title,String subTitle,String bottomName ,String imagePath){
+Widget _page( PageController pageController,int index,String title,String subTitle,String bottomName ,String imagePath,BuildContext context){
+
   return  Column(
     children: [
       SizedBox(height: 345.h,width: 345.w,
-        child: Text(imagePath),
+        child: Image.asset(imagePath,
+          fit: BoxFit.cover,
+
+        ),
       ),
       Container(
         padding: EdgeInsets.symmetric(horizontal: 24.w),
@@ -109,23 +125,36 @@ Widget _page( int index,String title,String subTitle,String bottomName ,String i
         ),
       ),
       SizedBox(height: 100.h,),
-      Container(
-        width: 325.w,
-        height: 50.h,
-        decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(24.r)
-        ),
-        child: Center(
-          child: Text(
-            bottomName,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.white.withOpacity(.5),
-                fontSize: 20.sp,
-                fontWeight: FontWeight.normal
-            ),
+      GestureDetector(
+        onTap: (){
+          if (index<3){
+            pageController.animateToPage(index,
+                duration: Duration(milliseconds: 500),
+                 curve:Curves.decelerate);
 
+          }else{
+            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>SignUpScreen()));
+
+          }
+        },
+        child: Container(
+          width: 325.w,
+          height: 50.h,
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(24.r)
+          ),
+          child: Center(
+            child: Text(
+              bottomName,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white.withOpacity(.5),
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.normal
+              ),
+
+            ),
           ),
         ),
       )
